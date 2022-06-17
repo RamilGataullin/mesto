ae3a298f584b9ae6186f1b73c3600554c4e789d0
Ramil Gataullin <rasim1922@icloud.com>
2022-06-18 00:58:11 +0300

    fixed:добавить кнопки, изменить разметку в popup, центрировать блоки

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const profileEditButton = document.querySelector('.profile__edit-button');
+const profileEditBtn = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupClose = document.querySelector('.popup__close');
 const nameInput = document.querySelector('.popup__input_type-name');
 const jobInput = document.querySelector('.popup__input_type-job');
 const profileName = document.querySelector('.profile__name');
 const profilejob = document.querySelector('.profile__job');
-const popupForm = document.querySelector('.popup__form');
+const formProfile = document.querySelector('.popup__form');
+
 
 function openPopup () {
     popup.classList.add('popup_opened');
@@ -24,6 +25,6 @@ function hadleSubmitProfile (evt) {
     closePopup();
 }
 
-profileEditButton.addEventListener('click', openPopup);
+profileEditBtn.addEventListener('click', openPopup);
 popupClose.addEventListener('click', closePopup);
-popupForm.addEventListener('submit', hadleSubmitProfile);
+formProfile.addEventListener('submit', hadleSubmitProfile);
